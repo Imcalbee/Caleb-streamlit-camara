6697e412badac9c855a8859aa4f7b233db0d4398
Imcalbee <chanyycaleb@gmail.com>
2024-09-12 16:54:52 +0800

    app.py

--- app.py
+++ app.py
@@ -82,4 +82,4 @@ if source_radio == "VIDEO":
         st.write("Click on 'Browse Files' in the sidebar to run inference on an video")
 
 if source_radio == "WEBCAM":
-    play_video(0)
+    play_live_camera()
